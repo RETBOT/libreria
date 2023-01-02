55c56d3c597872e7740af18f7e4731916e04c716
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 18:56:16 -0600

    Update views.py

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,10 +1,12 @@
+# by: RETBOT
 from audioop import reverse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import FormularioCreacionUsuario
-
+# by: RETBOT
 # Create your views here.
 class VistaPaginaRegistro(CreateView):
     form_class = FormularioCreacionUsuario
     success_url = reverse_lazy('login')
     template_name = 'registration/registro'
+    # by: RETBOT
