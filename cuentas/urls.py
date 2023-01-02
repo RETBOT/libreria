3e1fa3a19acc6a7b47d105829fdb14f94859358b
Roberto Esquivel Troncoso <71575985+RETBOT@users.noreply.github.com>
2023-01-01 19:08:51 -0600

    Update urls.py

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -1,8 +1,9 @@
 # cuentas/urls.py
 from django.urls import path
-
+# by: RETBOT
 from .views import VistaPaginaRegistro
 
 urlpatterns = [
     path('registro/', VistaPaginaRegistro.as_view(), name='registro'),
 ]
+# by: RETBOT
